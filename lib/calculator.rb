afe0bbd6b22a038f07421b59d8ef5ec7154bb59e
Li Yu <yu.li@rea-group.com>
2016-09-10 22:14:10 +0800

    calculator

---
+++ lib/calculator.rb
@@ -0,0 +1,63 @@
+
+class Calculator
+
+  def self.weight_of_certain_route(route_map, certain_route)
+    weight = 0
+    (0..certain_route.length-2).each do |index|
+      weight_tmp = route_map.weight_between_two_stations(certain_route[index], certain_route[index+1])
+      if weight_tmp == -1
+        weight = -1
+        break
+      end
+      weight += weight_tmp
+    end
+    weight==-1 ? 'NO SUCH ROUTE' : weight
+  end
+
+  def self.number_of_routes_with_max_stops(route_map, start_station, end_station, max_stop)
+    params = {:route_map => route_map, :end_station => end_station, :max_stop => max_stop, :number_of_routes => 0}
+    route_with_max_stop(start_station, 1, params)
+    params[:number_of_routes]
+  end
+
+  def self.weight_of_shortest_route(route_map, start_station, end_station)
+    params = {:route_map => route_map, :end_station => end_station, :weight => 1000}
+    shortest_route(start_station, '', 0, params)
+    params[:weight]
+  end
+
+  def self.number_of_routes_with_max_weight(route_map, start_station, end_station, max_weight)
+    params = {:route_map => route_map, :end_station => end_station, :max_weight => max_weight, :number_of_routes => 0}
+    route_with_max_weight(start_station,'', 0, params)
+    params[:number_of_routes]
+  end
+
+  private
+
+  def self.route_with_max_stop(current_station, current_stop, params)
+    return if current_stop > params[:max_stop]
+    params[:route_map].next_stations(current_station).each do |station|
+      route_with_max_stop(station, current_stop + 1, params)
+    end
+    params[:number_of_routes] += 1 if current_station == params[:end_station]
+  end
+
+  def self.shortest_route(current_station, current_route, weight, params)
+    if current_station == params[:end_station] && current_route != ''
+      params[:weight] = weight if weight < params[:weight]
+      return
+    end
+    return if weight > params[:weight]
+    params[:route_map].next_stations(current_station).each do |station|
+    shortest_route(station, current_route + station , weight + params[:route_map].weight_between_two_stations(current_station, station), params)
+    end
+  end
+
+  def self.route_with_max_weight(current_station, current_route, weight, params)
+    return if weight>= params[:max_weight]
+    params[:route_map].next_stations(current_station).each do |station|
+    route_with_max_weight(station, current_route + current_station, weight + params[:route_map].weight_between_two_stations(current_station, station), params)
+    end
+    params[:number_of_routes] += 1 if current_station == params[:end_station] && current_route != ''
+  end
+end
---
+++ spec/lib/caculator_spec.rb
@@ -0,0 +1,60 @@
+require 'route_mapper'
+require 'calculator'
+
+RSpec.describe Calculator do
+  let(:route_map) {RouteMapper.new(
+      {'A' => {'B' => 5, 'D' => 5, 'E' => 7},
+       'B' => {'C' => 4},
+       'C' => {'D' => 8, 'E' => 2},
+       'D' => {'C' => 8, 'E' => 6},
+       'E' => {'B' => 3}
+      }
+  )}
+  describe '#weight_of_certain_route' do
+    context 'with a right route' do
+      it 'should return weight' do
+        certain_route = 'ABC'
+        expect(Calculator.weight_of_certain_route(route_map, certain_route)).to eq(9)
+      end
+    end
+
+    context 'with a wrong route' do
+      it 'should return NO SUCH ROUTE' do
+        certain_route = 'ABD'
+        expect(Calculator.weight_of_certain_route(route_map, certain_route)).to eq('NO SUCH ROUTE')
+      end
+    end
+  end
+
+  describe '#number_of_routes_with_max_stops' do
+    context 'with max stop' do
+      it 'should return the number of routes between two stations' do
+        start_station = 'C'
+        end_station = 'C'
+        max_stop = 3
+        expect(Calculator.number_of_routes_with_max_stops(route_map,start_station,end_station,max_stop)).to eq(2)
+      end
+    end
+  end
+
+  describe '#weight_of_shortest_route' do
+    context 'with two stations' do
+      it 'should return weight of the shortest route' do
+        start_station = 'A'
+        end_station = 'C'
+        expect(Calculator.weight_of_shortest_route(route_map,start_station,end_station)).to eq(9)
+      end
+    end
+  end
+
+  describe '#number_of_routes_with_max_weight' do
+    context 'with max weight' do
+      it 'should return numbers of route between two stations' do
+        start_station = 'C'
+        end_station = 'C'
+        max_weight = 30
+        expect(Calculator.number_of_routes_with_max_weight(route_map,start_station,end_station,max_weight)).to eq(7)
+      end
+    end
+  end
+end

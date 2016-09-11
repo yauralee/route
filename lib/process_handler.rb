20246aee445ea48429cfe25071e314d1534c6a1a
Li Yu <yu.li@rea-group.com>
2016-09-12 01:31:09 +0800

    rake successfully

--- lib/process_handler.rb
+++ lib/process_handler.rb
@@ -1,7 +1,5 @@
-# require 'require_all'
-# require_all 'lib'
-
-Dir["#{File.dirname(__FILE__)}/*/*.rb", "#{File.dirname(__FILE__)}/*.rb"].each {|file| require file }
+require 'require_all'
+require_all 'lib'
 
 class ProcessHandler
   def self.process(routes_yml, requirement_yml)
@@ -12,5 +10,3 @@ class ProcessHandler
     Printer.print_result(result_array)
   end
 end
-
-ProcessHandler.process('../resource/route.yml', '../resource/calculation_requirement_and_condition.yml')

ad5f630963cecd390a22c24b12c0c75431560218
Li Yu <yu.li@rea-group.com>
2016-09-12 12:02:34 +0800

    change calss method

--- lib/result_printer.rb
+++ lib/result_printer.rb
@@ -1,5 +1,5 @@
 class Printer
-  def self.print_result(result_array)
+  def print_result(result_array)
     result_array.each_with_index do |result, counter|
       puts 'Output #' + counter.to_s + ' : ' + result
     end
